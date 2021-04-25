2a0c0e703aac9b1a485f49e61f0975c80d516df3
Xiaochen Xu <xiaochex@gmail.com>
2021-04-24 22:28:29 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    focusMessage = () => {
+        console.log('Good!')
+    }
+
+    blurMessage = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <div>
+                <button onFocus={this.focusMessage} onBlur={this.blurMessage}>
+                </button>
+            </div>
+        )
+    }
+}
